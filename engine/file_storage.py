218733b7c78eda9f88926ec8fdc335910373f0f0
= <=>
2021-02-15 18:46:12 +0200

    task 4 and 5

--- engine/file_storage.py
+++ engine/file_storage.py
@@ -1,10 +1,43 @@
+#!/usr/bin/python3
+"""
+File storage:  serializes instances to a JSON file and 
+    deserializes JSON file to instances:
+"""
+import json
+import os
+from models.base_model import BaseModel
 
 
 class FileStorage:
+    """
+    serializes instances to a JSON file and 
+    deserializes JSON file to instances.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        pass
-    def find(self, model, id):
-        pass
+        """return the class atribute objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = '{}.{}'.format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        file = FileStorage.__file_path
+        with open(file, mode="a", encoding="utf-8") as f:
+            f.write(json.dumps(FileStorage.__objects))
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        file = FileStorage.__file_path
+        if os.path.exists(file):
+            with open(file, mode="a", encoding="utf-8") as f:
+                pass
+
     def update(self, model, id, attr, value):
         pass
--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 base class file
 """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -13,22 +13,33 @@ class BaseModel:
     =========
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialize the instance of the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    self.__dict__[key] = value
+            if "created_at" in kwargs.keys():
+                self.created_at = datetime.strptime(kwargs["created_at"],
+                    '%Y-%m-%dT%H:%M:%S.%f')
+            if "updated_at" in kwargs.keys():
+                self.updated_at = datetime.strptime(kwargs["updated_at"],
+                    '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """save new informations to the class object"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """return dictionary representaton of the instance"""
         dict_repr = {}
         for key, value in self.__dict__.items():
             dict_repr[key] = value
-            if isinstance(value, datetime.datetime):
+            if isinstance(value, datetime):
                 dict_repr[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
         dict_repr["__class__"] = type(self).__name__
         return dict_repr
