e5d010c963fbe086d2839f83072b77eb485db009
= <=>
2021-02-17 18:20:12 +0200

    last test

---
+++ tests/test_models/test_encoder.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Test file for city class
+"""
+
+import unittest
+from models.base_model import BaseModelEncoder
+
+
+class TestClass(unittest.TestCase):
+    """Test cases"""
+
+    def test_encoder(self):
+        """test encoder"""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
